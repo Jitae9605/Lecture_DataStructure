0581e5388aba629677b0518314959097ae376ff0
Jitae9605 <dlwlxo3819@naver.com>
2022-02-22 14:22:49 +0900

    (Lecture)Add 01_warmingUP.c and 02_Linear_Search.c

--- test/source/01_WarmingUP.c
+++ test/source/01_WarmingUP.c
@@ -22,16 +22,62 @@ int main(void)
 	printf("가장 큰 수는 %d입니다.\n", res);		// res 출력
 	*/
 
-	// 2) 이름과 나이를 입력받아 출력
-	char name[20];
-	int age;
+	/*
+	// 2) 버퍼오류에 의한 입출력 오류
+	int age = 0;
+	char ch;
 
-	printf("이름 : ");
-	scanf("%s", name);
+	printf("해결전 1번\n");
+	printf("나이 : ");
+	scanf("%d", &age);
+	printf("기호 : ");
+	scanf("%c", &ch);
+
+	printf("기호 : %c, 나이 : %d\n\n",ch, age);		// 제대로 입/출력이 작동하지 않는다.
+
+
+	printf("해결전 2번\n");							// 입력받는 순서를 바꿈
+	printf("기호 : ");
+	scanf("%c", &ch);
 	printf("나이 : ");
 	scanf("%d", &age);
 
-	printf("이름 : %s, 나이 : %d", name, age);
+
+	printf("기호 : %c, 나이 : %d\n\n", ch, age);						// 제대로 출력된다.
+
+	// 숫자를 입력받을때는 상관이 없지만.
+	// 문자열/문자를 입력받고 출력할때는 입출력에서 문제가 된다. => ENTER가 버퍼에 남아있어서 그렇다!
+
+	// <해결방법>
+	// 버퍼를 비운다 -> getchar();를 사용해 한개의 문자를 읽어오고 그냥 버리면 버퍼가 비워진다!
+
+	printf("해결후\n");
+	printf("나이 : ");
+	scanf("%d", &age);
+	getchar();
+	printf("기호 : ");
+	scanf("%c", &ch);
+
+	printf("기호 : %c, 나이 : %d\n\n", ch, age);		// 제대로 입/출력이 작동하지 않는다.
+	*/
+
+	/*
+	// 3) 입력받은 숫자까지의 합을 구하는 프로그램
+	int num_3;
+	int sum_3 = 0;
+
+	printf("정수를 입력하세요 : ");
+	scanf("%d", &num_3);
+
+	for (int i = 0; i <= num_3; i++)
+	{
+		sum_3 += i;
+	}
+	printf("입력받은 정수 %d까지의 정수 합은 %d입니다.\n", num_3, sum_3);
+
+	*/
+
+	// 4)
 
 	return 0;
 }
---
+++ test/source/02_Linear_Search.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int LSearch(int ar[], int len, int target)		// 순차탐색알고리즘
+{
+	int i;
+	for (i = 0; i < len; i++)
+	{
+		if (ar[i] == target)
+			return i;							// 찾은 대상의 인뎃스값 반환
+	}
+	return -1;									// 찾지못했음을 의미하는 값 반환
+
+}
+
+int main(void)
+{
+	int arr[] = { 3, 5, 2, 4, 9 };
+	int idx;
+
+	idx = LSearch(arr, sizeof(arr) / sizeof(int), 4);		// arr배열에서 4를 찾고 4의 배열위치를 반환해 idx에 저장
+	if (idx == -1)
+	{
+		printf("탐색실패 \n");
+	}
+	else
+	{
+		printf("타겟 저장 인덱스 : %d \n", idx);
+	}
+
+	idx = LSearch(arr, sizeof(arr) / sizeof(int), 7);		// arr배열에서 7을 찾고 찾지못했으니 -1을 반환해 idx에 저장하고 탐색실패메세지를 출력
+	if (idx == -1)
+	{
+		printf("탐색실패 \n");
+	}
+	else
+	{
+		printf("타겟 저장 인덱스 : %d \n", idx);
+	}
+
+	return 0;
+}
